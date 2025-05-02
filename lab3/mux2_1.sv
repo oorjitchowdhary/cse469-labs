e22c7130d1433cb68143db8631d8c72600a5af51
Oorjit Chowdhary <oorjit.chowdhary@hotmail.com>
2025-05-02 15:49:22 -0700

    remove RTL in mux2_1

--- lab3/mux2_1.sv
+++ lab3/mux2_1.sv
@@ -2,7 +2,13 @@ module mux2_1 (out, i0, i1, sel);
 	output logic out;
 	input logic i0, i1, sel;
 	
-	assign out = (i0 & ~sel) | (i1 & sel);
+	logic sel_n, out0, out1;
+	
+	not n (sel_n, sel);
+	and a0 (out0, i0, sel_n); // i0 & ~sel
+	and a1 (out1, i1, sel); // i1 & sel
+	
+	or or_out (out, out0, out1);
 endmodule
 
 module mux2_1_testbench();
