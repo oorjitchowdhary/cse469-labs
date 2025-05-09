2745c4aa64034d3bb7855e6c20abd02a31d4d99a
Oorjit Chowdhary <oorjit.chowdhary@hotmail.com>
2025-05-09 01:19:34 -0700

    64bit mux2, gate delays, longer clock

--- lab3/adder.sv
+++ lab3/adder.sv
@@ -1,3 +1,5 @@
+`timescale 1ns/10ps
+
 module adder (
 	cout, sum,
 	a, b, cin
@@ -10,13 +12,13 @@ module adder (
 	// carry_out = (a & b) | (a & cin) | (b & cin)
 	
 	logic a_xor_b;
-	xor g1 (a_xor_b, a, b);
-	xor g2 (sum, a_xor_b, cin);
+	xor #50 g1 (a_xor_b, a, b);
+	xor #50 g2 (sum, a_xor_b, cin);
 	
 	logic ab, acin, bcin;
-	and g3 (ab, a, b);
-	and g4 (acin, a, cin);
-	and g5 (bcin, b, cin);
-	or g6 (cout, ab, acin, bcin);
+	and #50 g3 (ab, a, b);
+	and #50 g4 (acin, a, cin);
+	and #50 g5 (bcin, b, cin);
+	or #50 g6 (cout, ab, acin, bcin);
 
 endmodule
